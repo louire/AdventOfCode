ca7b7105775b9606af135ac62756c4a0cb7b9c11
Loui Recio <lreciopersonal@gmail.com>
2023-12-06 18:40:53 -0600

    save me

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,7 +1,8 @@
 mod part1;
 mod part2;
-
+#[warn(unused_must_use)]
 fn main(){
-    //part1::part1();
+
+    part1::part1();
     part2::part2();
 }
--- day1/src/part2.rs
+++ day1/src/part2.rs
@@ -1,4 +1,3 @@
-use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
@@ -8,33 +7,35 @@ pub fn part2() -> io::Result<()> {
     let file = File::open(&path)?;
     let reader = io::BufReader::new(file);
 
-    let mut word_to_digit: HashMap<&str, u32> = HashMap::new();
-    word_to_digit.insert("one", 1);
-    word_to_digit.insert("two", 2);
-    word_to_digit.insert("three", 3);
-    word_to_digit.insert("four", 4);
-    word_to_digit.insert("five", 5);
-    word_to_digit.insert("six", 6);
-    word_to_digit.insert("seven", 7);
-    word_to_digit.insert("eight", 8);
-    word_to_digit.insert("nine", 9);
-
     let mut total = 0;
 
     for line in reader.lines() {
         let line = line?;
 
-        let (first_digit, last_digit) = line.split_at(line.find(|c: char| c.is_digit(10)).unwrap_or(line.len()));
-        let first_digit = first_digit.trim();
-        let last_digit = last_digit.trim();
+        let digit_mappings = vec![
+            ("one", 1), ("two", 2), ("three", 3), ("four", 4), ("five", 5),
+            ("six", 6), ("seven", 7), ("eight", 8), ("nine", 9)
+        ];
 
-        let line: u32 = if !first_digit.is_empty() && !last_digit.is_empty() {
-            word_to_digit.get(first_digit).unwrap_or(&0) * 10
-                + word_to_digit.get(last_digit).unwrap_or(&0)
-        } else {
-            0
-        };
-        total += line;
+        let mut first_digit = None;
+        let mut last_digit = None;
+
+        for (digit_str, digit_val) in digit_mappings.iter() {
+            if let Some(start_idx) = line.find(digit_str) {
+                if first_digit.is_none() {
+                    first_digit = Some(digit_val);
+                }
+            }
+            if let Some(end_idx) = line.rfind(digit_str) {
+                if last_digit.is_none() {
+                    last_digit = Some(digit_val);
+                }
+            }
+        }
+
+        if let (Some(first), Some(last)) = (first_digit, last_digit) {
+            total += first + last;
+        }
     }
 
     println!("Total result for part 2 is: {}", total);
